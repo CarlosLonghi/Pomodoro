14717961fcb2736b45c711d7ba3f8cf1f369dae7
CarlosLonghi <carlos.longhi@digitalone.com.br>
2023-12-11 17:23:58 -0300

    feat: use props Styled Components in Button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,27 @@
+import { Button } from './components/Button'
+
 export function App() {
 
   return (
     <>
-      <h1>Olá :)</h1>
+      <Button 
+        variant='primary'
+      />
+
+      <Button 
+        variant='secondary'
+      />
+
+      <Button 
+        variant='danger'
+      />
+
+      <Button 
+        variant='success'
+      />
+
+      <Button/>
+
     </>
   )
 }
