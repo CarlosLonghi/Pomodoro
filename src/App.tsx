f809a3d10c0a8842d8cbc2a69d270ab0d481bec7
CarlosLonghi <carlos.longhi@digitalone.com.br>
2023-12-12 10:15:26 -0300

    build: uninstall styled-components and install tailwindcss and tailwind-variants. To define button variations

--- src/App.tsx
+++ src/App.tsx
@@ -4,24 +4,32 @@ export function App() {
 
   return (
     <>
-      <Button 
-        variant='primary'
-      />
+      <div className="flex flex-row items-center justify-center gap-6 h-screen bg-zinc-900">
+        <div className='flex flex-col items-center justify-center gap-3'>
+          <Button size='xs'>Primary xs</Button>
+          <Button size='sm'>Primary sm</Button>
+          <Button>Primary Default</Button>
+        </div>
 
-      <Button 
-        variant='secondary'
-      />
+        <div className='flex flex-col items-center justify-center gap-3'>
+          <Button secondary size='xs'>Secondary xs</Button>
+          <Button secondary size='sm'>Secondary sm</Button>
+          <Button secondary>Secondary Default</Button>
+        </div>
+        
+        <div className='flex flex-col items-center justify-center gap-3'>
+          <Button success size='xs'>Success</Button>
+          <Button success size='sm'>Success</Button>
+          <Button success size='default'>Success Default</Button>
+          <Button success className='w-32'>Success Custom</Button>
+        </div>
 
-      <Button 
-        variant='danger'
-      />
-
-      <Button 
-        variant='success'
-      />
-
-      <Button/>
-
+        <div className='flex flex-col items-center justify-center gap-3'>
+          <Button danger size='xs'>Danger xs</Button>
+          <Button danger size='sm' >Danger sm</Button>
+          <Button danger>Danger Default</Button>
+        </div>
+      </div>
     </>
   )
 }
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,15 +1,42 @@
-import { ButtonContainer, ButtonVariant } from './Button.styles'
+import { ComponentProps } from 'react'
+import { CheckCircle } from '@phosphor-icons/react'
+import { tv, VariantProps } from 'tailwind-variants'
 
-interface ButtonProps {
-  variant?: ButtonVariant
-}
+const button = tv({
+  base: 'bg-zinc-100 flex items-center justify-center rounded text-sm font-medium text-zinc-900 hover:bg-slate-400',
+  variants: {
+    size: {
+      default: 'h-10 px-4',
+      sm: 'h-8 px-3',
+      xs: 'h-6 px-2 text-xs'
+    },
+    // primary: {}, //A variação primary pode ser definida na base
+    secondary: {
+      true: 'bg-blue-400 hover:bg-blue-500',
+    },
+    success: {
+      true: 'bg-emerald-600 hover:bg-emerald-400',
+    },
+    danger: {
+      true: 'bg-red-600 hover:bg-red-500 text-white'
+    }
+  },
+  defaultVariants: {
+    size: 'default',
+    success: false
+  },
+})
 
-export function Button({ variant = 'primary' }: ButtonProps) {
+export type ButtonProps = ComponentProps<'button'> & VariantProps<typeof button>
+
+export function Button({ success, danger, secondary, size, className, ...props }: ButtonProps) {
   return (
-    <ButtonContainer
-      variant={variant}
+    <button
+      data-success={success}
+      className={button({ success, danger, secondary, size, className })}
+      {...props}
     >
-      Olá
-    </ButtonContainer>
+      {success ? <CheckCircle className='w-5 h-5'/> : props.children}
+    </button>
   )
 }
