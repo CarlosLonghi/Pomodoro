3cfb7e567edc113f88c5ed9db9f1395d348585a0
CarlosLonghi <carlos.longhi@digitalone.com.br>
2023-12-13 09:08:50 -0300

    remove Admin route

--- src/layouts/DefaultLayoutAdmin.tsx
+++
@@ -1,9 +0,0 @@
-import { Outlet } from 'react-router-dom'
-
-export function DefaultLayoutAdmin() {
-  return (
-    <div className="font-display h-screen bg-green-700">
-      <Outlet />
-    </div>
-  )
-}
