52723ce84df77d8e2a70c41683a9856a8a453d1a
CarlosLonghi <carlos.longhi@digitalone.com.br>
2023-12-15 14:51:58 -0300

    finished style Home page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,15 @@
 import { Timer, Scroll } from '@phosphor-icons/react'
 import { NavLink } from 'react-router-dom'
 
-const navLinkStyle =
-  'text-white border-b-2 pb-1 border-b-gray-500 hover:border-b-green-500 transition duration-200 ease-in-out outline-none focus:text-green-400'
-
 export function Header() {
   return (
     <header className="flex justify-end">
       <nav className="flex items-center gap-4">
-        <NavLink title="Tempo" to="/" className={navLinkStyle}>
+        <NavLink title="Tempo" to="/" className="nav-link">
           <Timer size={30} />
         </NavLink>
 
-        <NavLink title="Histórico" to="history" className={navLinkStyle}>
+        <NavLink title="Histórico" to="history" className="nav-link">
           <Scroll size={30} />
         </NavLink>
       </nav>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,5 @@
 import { Play } from '@phosphor-icons/react'
 
-const spanTimerStyle =
-  'py-8 px-4 bg-gray-500 rounded overflow-hidden cursor-default'
-
 export function Home() {
   return (
     <main className="flex justify-center h-full">
@@ -10,35 +7,43 @@ export function Home() {
         <div className="text-gray-100 text-base flex gap-2 items-center flex-wrap">
           <label htmlFor="task">Vou trabalhar em</label>
           <input
-            className="mx-2 border-b-2 border-gray-100 outline-none focus:border-green-500"
+            className="input-task mx-2 border-b-2 border-gray-100 outline-none focus:border-green-500"
             type="text"
             id="task"
+            list="task-suggestions"
+            placeholder="Dê um nome ao projeto"
           />
+          <datalist id="task-suggestions">
+            <option value="Projeto 1" />
+            <option value="Projeto 2" />
+            <option value="Projeto 3" />
+          </datalist>
 
           <label htmlFor="minutesAmount">durante</label>
           <input
-            className="mx-2 border-b-2 border-gray-100 outline-none focus:border-green-500"
+            className="input-minutes mx-2 w-16 border-b-2 border-gray-100 outline-none focus:border-green-500"
             type="number"
             id="minutesAmount"
+            placeholder="00"
+            step={5}
+            min={5}
+            max={120}
           />
 
           <span>minutos.</span>
         </div>
 
         <div className="flex items-center justify-between w-full font-timer text-gray-100 text-9xl">
-          <span className={spanTimerStyle}>0</span>
-          <span className={spanTimerStyle}>0</span>
+          <span className="span-timer">0</span>
+          <span className="span-timer">0</span>
           <span className="text-green-500 overflow-hidden cursor-default">
             :
           </span>
-          <span className={spanTimerStyle}>0</span>
-          <span className={spanTimerStyle}>0</span>
+          <span className="span-timer">0</span>
+          <span className="span-timer">0</span>
         </div>
 
-        <button
-          className="flex items-center justify-center gap-2 text-gray-100 px-8 py-4 w-full bg-green-500 rounded-lg"
-          type="submit"
-        >
+        <button className="button-timer-submit" type="submit">
           <Play size={30} />
           Iniciar
         </button>
