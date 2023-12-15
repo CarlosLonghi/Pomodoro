70e4c620b1b3ba5770d089b9fcc7fea5e73637df
CarlosLonghi <carlos.longhi@digitalone.com.br>
2023-12-15 17:06:57 -0300

    feat: create History page

--- src/pages/History.tsx
+++ src/pages/History.tsx
@@ -1,7 +1,43 @@
 export function History() {
   return (
-    <div>
-      <h1 className="text-4xl text-white text-center">History</h1>
-    </div>
+    <main className="flex-1 px-12 py-8 flex flex-col">
+      <h1 className="text-2xl text-gray-100 text-center">Histórico</h1>
+
+      <div className="flex-1 overflow-auto mt-8">
+        <table className="history-table w-full border-collapse">
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Inicio</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr>
+              <td>Tarefa exemplo</td>
+              <td>10 minutos</td>
+              <td>há 1 mês</td>
+              <td>Concluído</td>
+            </tr>
+
+            <tr>
+              <td>Tarefa exemplo</td>
+              <td>10 minutos</td>
+              <td>há 1 mês</td>
+              <td>Concluído</td>
+            </tr>
+
+            <tr>
+              <td>Tarefa exemplo</td>
+              <td>10 minutos</td>
+              <td>há 1 mês</td>
+              <td>Concluído</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </main>
   )
 }
