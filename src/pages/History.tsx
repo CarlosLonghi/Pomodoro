bef920f2f4a4d75018e78fc4f0d4129b0ff7008c
CarlosLonghi <carlos.longhi@digitalone.com.br>
2024-03-05 10:13:06 -0300

    feat: use Context in History

--- src/pages/History.tsx
+++ src/pages/History.tsx
@@ -1,4 +1,6 @@
 import { useContext } from 'react'
+import { formatDistanceToNow } from 'date-fns'
+import { ptBR } from 'date-fns/locale'
 import { StatusTask } from '../components/StatusTask'
 import { CyclesContext } from '../contexts/CyclesContext'
 
@@ -7,8 +9,6 @@ export function History() {
   return (
     <main className="flex-1 px-12 py-8 flex flex-col">
       <h1 className="text-2xl text-gray-100 text-center">Histórico</h1>
-
-      <pre>{JSON.stringify(cycles, null, 2)}</pre>
 
       <div className="flex-1 overflow-auto mt-8">
         <table className="w-full border-collapse">
@@ -22,26 +22,25 @@ export function History() {
           </thead>
 
           <tbody>
-            <tr>
-              <td>Tarefa 1</td>
-              <td>10 minutos</td>
-              <td>há 1 mês</td>
-              <StatusTask complete />
-            </tr>
-
-            <tr>
-              <td>Tarefa 2</td>
-              <td>10 minutos</td>
-              <td>há 1 mês</td>
-              <StatusTask progress />
-            </tr>
-
-            <tr>
-              <td>Tarefa 3</td>
-              <td>10 minutos</td>
-              <td>há 1 mês</td>
-              <StatusTask interrupt />
-            </tr>
+            {cycles.map((cycle) => {
+              return (
+                <tr key={cycle.id}>
+                  <td>{cycle.task}</td>
+                  <td>{cycle.minutesAmount} minutos</td>
+                  <td>
+                    {formatDistanceToNow(cycle.startDate, {
+                      addSuffix: true,
+                      locale: ptBR,
+                    })}
+                  </td>
+                  {cycle.finishedDate && <StatusTask complete />}
+                  {cycle.interruptedDate && <StatusTask interrupt />}
+                  {!cycle.finishedDate && !cycle.interruptedDate && (
+                    <StatusTask progress />
+                  )}
+                </tr>
+              )
+            })}
           </tbody>
         </table>
       </div>
